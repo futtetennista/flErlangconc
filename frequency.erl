38771eb0dc6c5d78ffb16ead26530967ba144e59
futtetennista <futtetennista@gmail.com>
2017-04-04 17:45:44 +0200

    Implement activity 1.18

--- frequency.erl
+++ frequency.erl
@@ -1,4 +1,4 @@
-%% Based on code from 
+%% Based on code from
 %%   Erlang Programming
 %%   Francecso Cesarini and Simon Thompson
 %%   O'Reilly, 2008
@@ -7,7 +7,8 @@
 %%   (c) Francesco Cesarini and Simon Thompson
 
 -module(frequency).
--export([init/0]).
+-export([init/0,start/0]).
+-include_lib("eunit/include/eunit.hrl").
 
 %% These are the start functions used to create and
 %% initialize the server.
@@ -28,8 +29,8 @@ loop(Frequencies) ->
       Pid ! {reply, Reply},
       loop(NewFrequencies);
     {request, Pid , {deallocate, Freq}} ->
-      NewFrequencies = deallocate(Frequencies, Freq),
-      Pid ! {reply, ok},
+      {NewFrequencies,Reply} = deallocate(Frequencies,Freq,Pid),
+      Pid ! {reply,Reply},
       loop(NewFrequencies);
     {request, Pid, stop} ->
       Pid ! {reply, stopped}
@@ -38,11 +39,47 @@ loop(Frequencies) ->
 %% The Internal Help Functions used to allocate and
 %% deallocate frequencies.
 
+allocate_test() ->
+    [
+     ?assertEqual(allocate_twice(1),{{[11],[{10,1}]},{error,multiple_frequencies}})
+    ].
+
+allocate_twice(Pid) ->
+    InitialState={[10,11],[]},
+    {NewState,_}=allocate(InitialState,Pid),
+    allocate(NewState,Pid).
+
 allocate({[], Allocated}, _Pid) ->
   {{[], Allocated}, {error, no_frequency}};
-allocate({[Freq|Free], Allocated}, Pid) ->
-  {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
+allocate(State={[Freq|Free], Allocated}, Pid) ->
+    case lists:keyfind(Pid,2,Allocated) of
+        false ->
+            {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}};
+        _ ->
+            {State,{error,multiple_frequencies}}
+    end.
 
-deallocate({Free, Allocated}, Freq) ->
-  NewAllocated=lists:keydelete(Freq, 1, Allocated),
-  {[Freq|Free],  NewAllocated}.
+deallocate_test() ->
+    [
+     ?assertEqual(deallocate({[10,11],[]},10,1),{{[10,11],[]},{error,frequency_unused}}),
+     ?assertEqual(deallocate_unused(1),{{[11],[{10,1}]},{error,frequency_unused}})
+    ].
+
+deallocate_unused(Pid) ->
+    InitialState={[10,11],[]},
+    {NewState,_}=allocate(InitialState,Pid),
+    deallocate(NewState,11,Pid).
+
+deallocate(State={Free,Allocated},Freq,Pid) ->
+    case lists:keyfind(Freq,1,Allocated) of
+        false ->
+            {State,{error,frequency_unused}};
+        {_,OwnerPid} when Pid/=OwnerPid ->
+            {State,{error,frequency_unused}};
+        _ ->
+            NewAllocated=lists:keydelete(Freq,1,Allocated),
+            {{[Freq|Free],NewAllocated},ok}
+    end.
+
+start() ->
+    register(frequency,spawn(frequency,init,[])).
